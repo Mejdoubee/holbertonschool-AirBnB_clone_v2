e3a6c30efa59dffd23aa2af2fb13ba813685f80e
Mejdoubee <6235@holbertonstudents.com>
2023-08-19 22:32:52 +0100

    Table

--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+import uuid
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    '''
+    Inherits from BaseModel and Base
+    '''
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete')
+
+    @property
+    def cities(self):
+        from models import storage
+        all_cities = storage.all(City)
+        return [city for city in all_cities.values()
+                if city.state_id == self.id]
