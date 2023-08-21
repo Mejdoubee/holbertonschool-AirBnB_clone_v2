1a890a3c7cefe3bbc20a58182ee1c9e06575124d
Mejdoubee <6235@holbertonstudents.com>
2023-08-21 18:50:43 +0100

    Updating __str__

--- models/base_model.py
+++ models/base_model.py
@@ -35,11 +35,12 @@ class BaseModel:
             self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        '''
-        Return the string representation of:
-        [<class name>] (<self.id>) <self.__dict__>
-        '''
-        return f"[{self.__class__.__name__}] ({self.id}) {self.to_dict()}"
+        '''Returns a string representation of the instance.'''
+        dict_copy = self.__dict__.copy()
+        dict_copy["created_at"] = self.created_at.strftime('%Y-%m-%d %H:%M:%S')
+        dict_copy["updated_at"] = self.updated_at.strftime('%Y-%m-%d %H:%M:%S')
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, dict_copy)
+
 
     def save(self):
         '''
