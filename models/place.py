211f9e0307ecde1a43bd23cd75c58bb03c2d6432
Mejdoubee <6235@holbertonstudents.com>
2023-08-22 00:48:05 +0100

    Update

--- models/place.py
+++ models/place.py
@@ -20,3 +20,4 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+    reviews = relationship("Review", cascade="all, delete", backref="place")
