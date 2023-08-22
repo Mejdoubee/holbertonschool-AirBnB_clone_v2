76acc255a6191864ae2ba53f43cd4be749eef205
Mejdoubee <6235@holbertonstudents.com>
2023-08-23 00:02:51 +0100

    adding review to ORM

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -9,6 +9,7 @@ from models.state import State
 from models.city import City
 from models.user import User
 from models.place import Place
+from models.review import Review
 import os
 
 
@@ -43,7 +44,7 @@ class DBStorage:
         if cls:
             objects = self.__session.query(cls).all()
         else:
-            classes = [State, City, User, Place]
+            classes = [State, City, User, Place, Review]
             for cls in classes:
                 objects.extend(self.__session.query(cls).all())
 
