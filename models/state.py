2b6993a78fc0c41e29b5e070981ff3dd25ed2c46
Mejdoubee <6235@holbertonstudents.com>
2023-08-27 18:39:37 +0100

    Update state

--- models/state.py
+++ models/state.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+'''
+State Module for HBNB project'''
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
 
 class State(BaseModel, Base):
-    """ State class """
+    '''State class'''
+    storageType = getenv("HBNB_TYPE_STORAGE")
     __tablename__ = "states"
-
-    # Attributes
     name = Column(String(128), nullable=False)
 
-    # Relationships & Properties
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        cities = relationship('City', cascade="all, delete-orphan", backref='state')
+    if storageType == 'db':
+        cities = relationship('City',
+                              cascade="all, delete-orphan",
+                              backref='state'
+                              )
+
     else:
         @property
         def cities(self):
-            """Get all City objects associated with this State."""
+            '''Get all City objects associated with this State'''
             from models import storage
-            from models.city import City
-            
-            all_cities = storage.all(City)
-            state_cities = [city for city in all_cities.values() if city.state_id == self.id]
-            return state_cities
+
+            citiess = []
+            for value in storage.all(City).values():
+                if value.state_id == self.id:
+                    citiess.append(value)
+            return citiess
