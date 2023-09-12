19eef0f8314f9193aefa32c0057de274d1fbe865
Mejdoubee <6235@holbertonstudents.com>
2023-09-12 01:24:35 +0100

    States and State

---
+++ web_flask/9-states.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+'''
+script that starts a Flask web application
+'''
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def states_list():
+    '''
+    Display a list of all states
+    '''
+    states_sorted = sorted(
+        storage.all(State).values(), key=lambda state: state.name
+    )
+    return render_template('8-cities_by_states.html', states=states_sorted)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_and_cities(id):
+    '''
+    Display a state and its cities
+    '''
+    state = storage.get(State, id)
+    if state:
+        cities_sorted = sorted(state.cities, key=lambda city: city.name)
+        return render_template(
+            "9-states.html", state=state, cities=cities_sorted
+        )
+    else:
+        return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown_db(exception=None):
+    '''
+    Close the session after each request
+    '''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
